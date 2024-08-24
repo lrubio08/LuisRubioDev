f88fd93453a252407e3eb95430466968ed71f53d
lrubio08 <lrubio1179@gmail.com>
2024-08-24 11:48:21 -0400

    Primer commit: subiendo mi portafolio a GitHub

---
+++ README.md
@@ -0,0 +1,38 @@
+# Luis Rubio | Portafolio Web
+
+## Descripción
+
+El propósito principal de este portafolio es demostrar mis habilidades como desarrollador web y así poder conseguir un empleo en el área, ya sea como freelancer o como empleado dependiente. Está dirigido principalmente a reclutadores del área de tecnología, así como al público en general que necesite una presencia en línea.
+
+## Características Principales
+
+- **Modos Oscuro y Claro**: El portafolio cuenta con la funcionalidad de cambiar entre modos oscuro y claro según la preferencia del usuario.
+- **Formulario de Contacto**: Incluye un formulario de contacto manejado con EmailJS para el envío de correos directamente a mi dirección de correo electrónico.
+- **Alertas de Confirmación**: Uso de SweetAlert para mostrar alertas de confirmación al enviar el formulario de contacto.
+- **Secciones del Portafolio**:
+  - **Principal**: Sección de bienvenida con mi nombre y área de especialización ("Desarrollador Web").
+  - **Sobre Mí**: Muestra una imagen personal junto con una breve descripción de mi perfil.
+  - **Habilidades**: Descripción de las tecnologías que domino, incluyendo HTML, CSS, JavaScript, Python, Django, PostgreSQL, Bootstrap, entre otras.
+  - **Contacto**: Un formulario que permite a los usuarios enviar su nombre, apellido, correo electrónico y mensaje, el cual llega directamente a mi correo a través de EmailJS.
+
+## Tecnologías Utilizadas
+
+- **HTML**
+- **CSS**
+- **JavaScript**
+- **Bootstrap**
+- **APIs Externas**:
+  - EmailJS
+  - SweetAlert
+
+## Instrucciones de Instalación y Uso
+
+Para ver el portafolio, puedes:
+
+1. Abrir el archivo `index.html` en cualquier navegador web.
+2. O visitar la versión en línea del portafolio a través de [GitHub Pages](https://github.com/tu-usuario/mi-portafolio).
+
+## Créditos
+
+Portafolio creado por Luis Rubio como una demostración de habilidades en desarrollo web.
+
---
+++ assets/js/scripts.js
@@ -0,0 +1,85 @@
+// cierra el menu cuando el usario hace click en uno de sus enlaces 
+function closeMenu() {
+    let navbarCollapse = document.querySelector('.navbar-collapse');
+    if (navbarCollapse.classList.contains('show')) {
+        navbarCollapse.classList.remove('show');
+    }
+}
+
+//Mostrar curriculum
+function mostrarCurriculum() {
+    window.open("/assets/documentos/Cv Luis Rubio.pdf", "_blank");
+}
+
+//descargar curriculum
+function descargarCV() {
+    var a = document.createElement('a');
+    a.href = "/assets/documentos/Cv Luis Rubio.pdf";
+    a.download = "Cv Luis Rubio.pdf";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+//Envio de correos
+
+
+// Inicializa EmailJS al cargar la página
+emailjs.init("Tbv4Ah_0_UfyfyscX");
+
+// ID del servicio y plantilla que usará EmailJS para enviar el correo
+const serviceId = "service_yvec8hp";
+const templateId = "template_0mln2jt";
+
+
+// Maneja el envío del formulario de contacto
+function enviarEmail(event) {
+    event.preventDefault(); // Evita que el formulario se envíe de manera tradicional
+
+    // Envía el formulario utilizando EmailJS
+    emailjs.sendForm(serviceId, templateId, formularioContacto)
+        .then(() => {
+            // Muestra un mensaje de éxito
+            Swal.fire("¡Su mensaje ha sido enviado con éxito!");
+            formularioContacto.reset(); // Limpia el formulario después de enviarlo
+        })
+        .catch((error) => {
+            // Manejo de errores
+            console.error('Error al enviar el correo:', error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Hubo un error al enviar el mensaje',
+                text: "No ha sido posible enviar su mensaje"
+            });
+        });
+}
+
+//Se obtiene el formulario y agrega el evento 'submit' para manejar el envío del formulario
+const formularioContacto = document.getElementById("formContact");
+formularioContacto.addEventListener("submit", enviarEmail);
+
+
+//manejo de modo oscuro y light 
+const toggleTheme = () => {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+
+    // Cambiar la clase del navbar dependiendo del tema
+    const navbar = document.querySelector('.navbar');
+    if (newTheme === "dark") {
+        navbar.classList.add('navbar-dark');
+        navbar.classList.remove('navbar-light');
+    } else {
+        navbar.classList.add('navbar-light');
+        navbar.classList.remove('navbar-dark');
+    }
+
+}
+
+document.getElementById("theme-toggle").addEventListener("click", toggleTheme);
+
+// Al cargar la página, verificar la preferencia guardada
+const savedTheme = localStorage.getItem("theme") || "light";
+document.documentElement.setAttribute("data-theme", savedTheme);
